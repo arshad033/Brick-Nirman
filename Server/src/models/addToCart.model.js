f753eecf89ca7566e61b62dc43cc63effbc8664d
anuj407 <anuj97953a@gmail.com>
2025-03-31 00:57:42 +0530

    write the controllers for addToCart controller

--- Server/src/models/addToCart.model.js
+++ Server/src/models/addToCart.model.js
@@ -7,24 +7,20 @@ const addToCartSchema = new mongoose.Schema(
       ref: 'User',
       required: true,
     },
-    products: [
-      {
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          default: 1,
-        },
-        price: {
-          type: Number,
-          required: true,
-        },
-      },
-    ],
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
   },
   { timestamps: true }
 );
