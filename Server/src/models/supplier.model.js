1b9fb5fbd79effd7b9ac14846f6b8f8fcc1a7c60
arshad033 <arshadobra85@gmail.com>
2025-03-31 00:11:11 +0530

    changes in supplier model

--- Server/src/models/supplier.model.js
+++ Server/src/models/supplier.model.js
@@ -1,70 +1,70 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const supplierSchema = new mongoose.Schema({
+const supplierSchema = new mongoose.Schema(
+  {
     supplierId: {
-        type: String,
-        required: true,
-        unique: true
+      type: Schema.Types.ObjectId,
+      ref: 'User',
     },
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     registrationNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     gstNumber: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     contactPerson: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     address: {
-        street: { type: String, required: true },
-        city: { type: String, required: true },
-        state: { type: String, required: true },
-        zip: { type: String, required: true },
-        country: { type: String, required: true, default: "India" },
-        geoLocation: {
-            latitude: { type: Number },
-            longitude: { type: Number }
-        }
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      zip: { type: String, required: true },
+      country: { type: String, required: true, default: 'India' },
+      geoLocation: {
+        latitude: { type: Number },
+        longitude: { type: Number },
+      },
     },
     product: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     serviceArea: {
-        type: [String]  // Array of service areas
+      type: [String], // Array of service areas
     },
     paymentMethods: {
-        type: [String],  // Array of accepted payment methods
-        default: ["Cash"]
+      type: [String], // Array of accepted payment methods
+      default: ['Cash'],
     },
     paymentTerms: {
-        type: String
+      type: String,
     },
     ratings: {
-        type: Number,
-        min: 0,
-        max: 5
-    }
+      type: Number,
+      min: 0,
+      max: 5,
+    },
   },
   {
-    timestamps: true
+    timestamps: true,
   }
 );
 
-export const Supplier = mongoose.model("Supplier", supplierSchema);
+export const Supplier = mongoose.model('Supplier', supplierSchema);
