002a4df94b4935e5778041176286e28da8c24068
anuj407 <anuj97953a@gmail.com>
2025-03-31 01:22:28 +0530

    update model

--- Server/src/models/addToFav.model.js
+++ Server/src/models/addToFav.model.js
@@ -3,9 +3,8 @@ import mongoose, { Schema } from 'mongoose';
 const addToFavSchema = new Schema(
   {
     userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    // Assuming you are favoriting suppliers or specific products
-    supplierId: { type: Schema.Types.ObjectId, ref: 'User', required: false },
-    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: false },
+    supplierId: { type: Schema.Types.ObjectId, ref: 'User' },
+    productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
   },
   { timestamps: true }
 );
