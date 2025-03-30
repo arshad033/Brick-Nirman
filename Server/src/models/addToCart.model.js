a1a85ff7af922c7a6d3fbdddc9b93a0895d73992
arshad033 <arshadobra85@gmail.com>
2025-03-30 15:07:49 +0530

    added addtocart & user model

---
+++ Server/src/models/addToCart.model.js
@@ -0,0 +1,32 @@
+import mongoose, { Schema } from 'mongoose';
+
+const addToCartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    products: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Product',
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          default: 1,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const AddToCart = mongoose.model('AddToCart', addToCartSchema);
--- Server/src/models/user.model.js
+++ Server/src/models/user.model.js
@@ -1,1 +1,56 @@
-``
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+const userSchema = new Schema(
+  {
+    fullName: { type: String, required: true, index: true },
+    email: { type: String, required: true, unique: true, lowercase: true },
+    phone: { type: String, required: true, unique: true },
+    address: {
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String, required: true },
+      country: { type: String, required: true },
+      zipCode: { type: String, required: true },
+    },
+    avatar: { type: String },
+    role: {
+      type: String,
+      enum: ['user', 'seller', 'admin'],
+      default: 'user',
+    },
+    password: { type: String, required: [true, 'password is required'] },
+    refreshToken: { type: String },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  //   console.log("runnig pre");-
+
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+export const User = mongoose.model('User', userSchema);
