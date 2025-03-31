cdc285b69207f21b65dc9570f6ec87d1328587ee
anuj407 <anuj97953a@gmail.com>
2025-03-31 13:35:35 +0530

    create Routes for order Controller

--- Server/src/routes/order.routes.js
+++ Server/src/routes/order.routes.js
@@ -3,6 +3,11 @@ const router = Router()
 import { createOrder, getAllOrders, getOrderById, updateOrder, deleteOrder } from "../controllers/order.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
+router.use(verifyJwt)
+router.route("/create-order").post(createOrder);
+router.route("/get-All-Orders").get(getAllOrders);
+router.route("/update-orders").put(getAllOrders);
+router.route("/delete-orders").delete(getAllOrders);
+router.route("/get-order/:id").get(getOrderById);
 
-router.route("/create-order").post(verifyJwt,createOrder);
-router.route("/get-All-Orders").get(verifyJwt,getAllOrders);
+export default router;
