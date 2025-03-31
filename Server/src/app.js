2ad60ab9f430586b58c6c806f5d962ce0e3700ab
anuj407 <anuj97953a@gmail.com>
2025-03-31 13:25:02 +0530

    add routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -2,6 +2,9 @@ import express, { urlencoded } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
+import orderRouter from "./routes/order.routes.js"
+import productRouter from "./routes/product.routes.js"
+
 
 app.use(
   cors({
@@ -15,5 +18,7 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 //import Route
+app.use('/api/v1/orders',orderRouter);
+app.use('/api/v1/products',productRouter)
 
 export { app };
