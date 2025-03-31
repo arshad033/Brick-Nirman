3297280bfd9046d163487dadbb9b940e1762fbf9
anuj407 <anuj97953a@gmail.com>
2025-03-31 13:34:58 +0530

    create Routes for addToFav Controller

---
+++ Server/src/routes/addToFav.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+const router = Router();
+import { 
+    addToFavorites, 
+    getFavorites, 
+    removeFromFavorites 
+} from "../controllers/addToCart.controller.js";
+import { verifyJwt } from "../middlewares/auth.middleware.js";
+
+// ✅ Protect all routes with JWT authentication
+router.use(verifyJwt);
+
+// ✅ Routes
+router.route("/add-to-favorites").post(addToFavorites);
+router.route("/get-favorites").get(getFavorites);
+router.route("/remove-from-favorites").delete(removeFromFavorites);
+
+export default router;
