f2f052c33292d436052dfb76177bbde8cd4ac6b7
anuj407 <anuj97953a@gmail.com>
2025-04-01 00:06:40 +0530

    api testing and debuging

--- Server/src/routes/product.routes.js
+++ Server/src/routes/product.routes.js
@@ -8,14 +8,14 @@ import {
     deleteProduct 
 } from "../controllers/product.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
-
+import { upload } from "../middlewares/multer.middleware.js";
 // ✅ Protect all routes with JWT authentication
 router.use(verifyJwt);
 
 // ✅ Routes
-router.route("/create-product").post(createProduct);
+router.route("/create-product").post(upload.single('image'),createProduct);
 router.route("/get-all-products").get(getAllProducts);
-router.route("/update-product/:id").put(updateProduct);
+router.route("/update-product/:id").patch(updateProduct);
 router.route("/delete-product/:id").delete(deleteProduct);
 router.route("/get-product/:id").get(getProductById);
 
