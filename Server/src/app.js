b8148205a6dc7fa6ce79e5ce88e54453d69817d2
anuj407 <anuj97953a@gmail.com>
2025-03-31 13:50:57 +0530

    add routes

--- Server/src/app.js
+++ Server/src/app.js
@@ -4,7 +4,8 @@ import cookieParser from "cookie-parser";
 const app = express();
 import orderRouter from "./routes/order.routes.js"
 import productRouter from "./routes/product.routes.js"
-
+import AddToFavRouter from "./routes/addToFav.routes.js"
+import AddToCartRouter from "./routes/addToCart.routes.js"
 
 app.use(
   cors({
@@ -20,5 +21,8 @@ app.use(cookieParser());
 //import Route
 app.use('/api/v1/orders',orderRouter);
 app.use('/api/v1/products',productRouter)
+app.use('/api/v1/addToFav',AddToFavRouter)
+app.use('/api/v1/addToCart',AddToCartRouter)
+
 
 export { app };
