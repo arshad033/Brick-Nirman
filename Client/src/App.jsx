015b3e73324907b98b6e22171469f91c669a6832
arshad033 <arshadobra85@gmail.com>
2025-04-02 00:42:29 +0530

    j

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import { AppContext } from './context/AppContext';
-import Dashboard from './components/Dashboard';
+import React from "react";
+import Order from "./pages/Order";
+import Checkout from "./pages/Checkout";
+import Login from "./components/Login";
+// import { AppContext } from "./context/AppContext";
+// import Dashboard from "./components/Dashboard";
 
 function App() {
-
-    return (
-        <div>
-            <Dashboard/>
-        </div>
-    );
+  return <div></div>;
 }
 
-export default App
+export default App;
