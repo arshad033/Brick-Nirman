c71c7e4f0fec83a2e56f4a9da5e2b180121a2d1e
anuj407 <anuj97953a@gmail.com>
2025-04-02 00:26:29 +0530

    initialize react router

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { AppContext } from './context/AppContext';
-import Dashboard from './components/Dashboard';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 
     return (
-        <div>
-            <Dashboard/>
+        <div className="overflow-x-hidden bg-gray-900">
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
 }
