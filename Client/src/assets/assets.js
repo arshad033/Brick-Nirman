6cae915fb7dbbd8e29ceb5a2befdf2e01fd1675a
anuj407 <anuj97953a@gmail.com>
2025-04-01 13:44:59 +0530

    add assets

---
+++ Client/src/assets/assets.js
@@ -0,0 +1,11 @@
+import slider_img1 from './images/slider_img1.png';
+import slider_img2 from './images/slider_img2.png';
+import slider_img3 from './images/slider_img3.png';
+import slider_img4 from './images/slider_img4.jpg';
+
+export {
+    slider_img1,
+    slider_img2,
+    slider_img3,
+    slider_img4
+}
