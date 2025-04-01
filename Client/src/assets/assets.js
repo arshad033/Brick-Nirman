ae6f69bc7e15a77c9894548c1e779dd6b06a9a4e
anuj407 <anuj97953a@gmail.com>
2025-04-02 00:29:21 +0530

    add assets

--- Client/src/assets/assets.js
+++ Client/src/assets/assets.js
@@ -2,10 +2,20 @@ import slider_img1 from './images/slider_img1.png';
 import slider_img2 from './images/slider_img2.png';
 import slider_img3 from './images/slider_img3.png';
 import slider_img4 from './images/slider_img4.jpg';
+import logo from './images/logo.png';
+import { IoMdMenu } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
+import { FaCartShopping } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
 
 export {
     slider_img1,
     slider_img2,
     slider_img3,
-    slider_img4
+    slider_img4,
+    logo,
+    IoMdMenu,
+    IoCloseSharp,
+    FaCartShopping,
+    FaHeart
 }
