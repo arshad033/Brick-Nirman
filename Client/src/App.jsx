3caf0528a87295744c4bd5a56328251be51b0323
anuj407 <anuj97953a@gmail.com>
2025-04-02 00:45:31 +0530

    update

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,13 +1,17 @@
-import React from "react";
-import { AppContext } from "./context/AppContext";
-import Dashboard from "./components/Dashboard";
+import React from 'react'
+import { AppContext } from './context/AppContext';
+import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
+
+    return (
+        <div className="overflow-x-hidden bg-gray-900">
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </div>
+    );
 }
 
-export default App;
+export default App
