47392815ddf695bd3fe8cc54e645545d701a499f
anuj407 <anuj97953a@gmail.com>
2025-04-03 01:48:01 +0530

    add routing for supplier, product, order, and checkout pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
-import { AppContext } from './context/AppContext';
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
+import SupplierPage from "./pages/SupplierPage.jsx"
+import ProductPage from "./pages/ProductPage.jsx"
+import Order from "./pages/Order.jsx"
+import Checkout from "./pages/Checkout.jsx"
 
 function App() {
 
@@ -9,6 +12,10 @@ function App() {
         <div className="overflow-x-hidden bg-gray-900">
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/suppliers" element={<SupplierPage />} />
+                <Route path="/products" element={<ProductPage />} />
+                <Route path="/orders" element={<Order />} />
+                <Route path="/buy-checkout" element={<Checkout />} />
             </Routes>
         </div>
     );
