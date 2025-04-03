afb2475bedc29aae0a9dcc9e36b6134fcf53671f
arshad033 <arshadobra85@gmail.com>
2025-04-03 12:28:55 +0530

    app push

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import { AppContext } from './context/AppContext';
-import Home from './pages/Home';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import { AppContext } from "./context/AppContext";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+
+import UserRegistration from "./components/UserRegistration";
+import SupplierRegistration from "./components/SupplierRegistration";
+import SupplierCard from "./components/SupplierCard";
+import SupplierPage from "./pages/SupplierPage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
-
-    return (
-        <div className="overflow-x-hidden bg-gray-900">
-            <Routes>
-                <Route path="/" element={<Home />} />
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="overflow-x-hidden bg-gray-900">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
