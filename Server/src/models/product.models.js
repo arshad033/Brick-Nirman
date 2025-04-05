0d382614c44d88fa9d44c7916841db135b316bfc
arshad033 <arshadobra85@gmail.com>
2025-04-05 12:48:05 +0530

    format product model schema

--- Server/src/models/product.models.js
+++ Server/src/models/product.models.js
@@ -1,65 +1,70 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     category: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     quantityAvailable: {
-        type: Number,
-        required: true,
-        min: 0
+      type: Number,
+      required: true,
+      min: 0,
     },
     size: {
-        type: String,    // e.g., "9x4x3 inches"
-        required: true
+      type: String, // e.g., "9x4x3 inches"
+      required: true,
     },
     grade: {
-        type: String,    // e.g., "A", "B", "C"
-        required: true
+      type: String, // e.g., "A", "B", "C"
+      required: true,
     },
     image: {
-        type: String,  // Array of image URL
+      type: [String], // Array of image URL
     },
     supplierId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Supplier",
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Supplier',
+      required: true,
     },
-    reviews: [{
-        userId: { 
-            type: mongoose.Schema.Types.ObjectId, 
-            ref: "User" 
+    reviews: [
+      {
+        userId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
         },
-        comment: { 
-            type: String 
+        comment: {
+          type: String,
         },
-        rating: { 
-            type: Number, 
-            min: 1, 
-            max: 5 
+        rating: {
+          type: Number,
+          min: 1,
+          max: 5,
         },
-        createdAt: { 
-            type: Date, 
-            default: Date.now 
-        }
-    }]
-}, {
-    timestamps: true   
-});
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
-export const Product = mongoose.model("Product", productSchema);
+export const Product = mongoose.model('Product', productSchema);
