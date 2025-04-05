0863d15e48184a99555653f5925ef05a0e65f1a5
anuj407 <anuj97953a@gmail.com>
2025-04-05 13:39:00 +0530

    refactor: implement logout functionality and update sidebar state management

--- Client/src/components/Sidebar.jsx
+++ Client/src/components/Sidebar.jsx
@@ -1,14 +1,54 @@
 import { FaHome, FaUsers, FaFile, FaHeart } from "react-icons/fa";
 import { IoCloseSharp, logo, FaCartShopping } from "../assets/assets.js";
+import { logoutUser } from "../utils/HandleAPIs.jsx";
+import { useContext, useEffect, useState } from "react";
+import { AppContext } from "../context/AppContext.jsx";
 
-export default function Sidebar({ isSidebarOpen, setIsSidebarOpen, sideNavSelect, handleSideNav }) {
+export default function Sidebar({
+  isSidebarOpen,
+  setIsSidebarOpen,
+  sideNavSelect,
+  handleSideNav,
+}) {
+  const [userLogout, setResponse] = useState(null);
+  const { setUser } = useContext(AppContext);
+  const handleLogout = () => {
+    logoutUser(setResponse);
+  };
+
+  useEffect(() => {
+    if (userLogout?.statusCode === 200) {
+      setIsSidebarOpen(false);
+      setUser(null);
+      setResponse(null);
+    }
+  }, [setIsSidebarOpen, setUser, userLogout]);
+
+  // useEffect(() => {
+  //   if (isSidebarOpen) {
+  //     document.body.addEventListener("click", function () {
+  //       setIsSidebarOpen(false);
+  //     });
+  //   }
+  // });
   return (
-    <aside className={`${isSidebarOpen ? "left-0" : "-left-[100%]"} bg-gray-900 w-[16rem] py-5 text-white flex flex-col overflow-hidden transition-all duration-500 fixed h-full z-40`}>
+    <aside
+      className={`${
+        isSidebarOpen ? "left-0" : "-left-[100%]"
+      } bg-gray-900 w-[16rem] py-5 text-white flex flex-col overflow-hidden transition-all duration-500 fixed h-full z-40`}
+    >
       <div className="flex items-center justify-between px-5">
         <div className="w-[40%] h-[3rem]">
-          <img className="w-full h-full scale-[1.5] object-cover" src={logo} alt="Logo" />
+          <img
+            className="w-full h-full scale-[1.5] object-cover"
+            src={logo}
+            alt="Logo"
+          />
         </div>
-        <div onClick={() => setIsSidebarOpen(false)} className="text-3xl font-bold cursor-pointer">
+        <div
+          onClick={() => setIsSidebarOpen(false)}
+          className="text-3xl font-bold cursor-pointer"
+        >
           <IoCloseSharp />
         </div>
       </div>
@@ -24,7 +64,9 @@ export default function Sidebar({ isSidebarOpen, setIsSidebarOpen, sideNavSelect
             <li
               key={item.name}
               onClick={() => handleSideNav(item.name)}
-              className={`${sideNavSelect === item.name && "bg-gray-800"} p-3 cursor-pointer flex items-center space-x-3 hover:bg-gray-800 rounded-md`}
+              className={`${
+                sideNavSelect === item.name && "bg-gray-800"
+              } p-3 cursor-pointer flex items-center space-x-3 hover:bg-gray-800 rounded-md`}
             >
               {item.icon} <span>{item.name}</span>
             </li>
@@ -32,7 +74,10 @@ export default function Sidebar({ isSidebarOpen, setIsSidebarOpen, sideNavSelect
         </ul>
       </nav>
       <div className="w-full border-t-[1px] px-5 border-gray-700">
-        <div className="p-3 flex items-center space-x-3 hover:bg-gray-800 rounded-md cursor-pointer">
+        <div
+          onClick={() => handleLogout()}
+          className="p-3 flex items-center space-x-3 hover:bg-gray-800 rounded-md cursor-pointer"
+        >
           <span>Logout</span>
         </div>
       </div>
