1e229fdd2c49048c384ea59a72e9ef20a4103eca
arshad033 <arshadobra85@gmail.com>
2025-04-06 22:43:26 +0530

    refactor: clean up commented code and maintain consistent formatting in App component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -11,29 +11,31 @@ import Favourites from "./pages/Favourites.jsx";
 import ContactUs from "./pages/ContactUs.jsx";
 import AboutUs from "./pages/AboutUs.jsx";
 import Cart from "./pages/Cart.jsx";
-import SupplierProfile from "./pages/SupplierProfile.jsx";
+// import SupplierProfile from "./pages/SupplierProfile";
+import ProductDetailsPage from "./pages/ProductDetailsPage.jsx";
 
 function App() {
-    return (
-        <div className="overflow-x-hidden bg-gray-900">
-            <Dashboard/>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/suppliers" element={<SupplierPage />} />
-                <Route path="/products" element={<ProductPage />} />
-                <Route path="/orders" element={<Order />} />
-                <Route path="/buy-checkout" element={<Checkout />} />
-                <Route path="/favourites" element={<Favourites />} />
-                <Route path="/contact-us" element={<ContactUs />} />
-                <Route path="/about-us" element={<AboutUs />} />
-                <Route path="/carts" element={<Cart />} />
-                <Route path="/supplier-profile" element={<SupplierProfile />} />
-            </Routes>
-            <div className="pt-10">
-              <Footer/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="overflow-x-hidden bg-gray-900">
+      <Dashboard />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/suppliers" element={<SupplierPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/orders" element={<Order />} />
+        <Route path="/buy-checkout" element={<Checkout />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/carts" element={<Cart />} />
+        {/* <Route path="/supplier-profile" element={<SupplierProfile />} /> */}
+        <Route path="/product/:id" element={<ProductDetailsPage />} />
+      </Routes>
+      <div className="pt-10">
+        <Footer />
+      </div>
+    </div>
+  );
 }
 
 export default App;
