3c113728edbd85e6b2730ab18fe959c9b6daa428
arshad033 <arshadobra85@gmail.com>
2025-04-07 16:49:51 +0530

    feat: update default user avatar to a more neutral placeholder image

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -151,7 +151,8 @@ export default function Navbar({
           >
             <img
               src={
-                user?.avatar || "https://randomuser.me/api/portraits/men/1.jpg"
+                user?.avatar ||
+                "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
               }
               alt="User"
               className="w-8 h-8 rounded-full"
