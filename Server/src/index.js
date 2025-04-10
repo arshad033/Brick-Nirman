b36ecf1a4c6dbcad17424e03eaa90ffbed855e60
arshad033 <arshadobra85@gmail.com>
2025-04-10 23:41:23 +0530

    fix: add root route handler to index.js for server status

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,9 +1,12 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 // import express from "express";
-import { connectDB } from "./db/db.js";
-import { app } from "./app.js";
+import { connectDB } from './db/db.js';
+import { app } from './app.js';
 dotenv.config();
 connectDB();
 
 const PORT = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+  res.send('Server is running');
+});
 app.listen(PORT, () => console.log(`server is listening on Port : ${PORT}`));
