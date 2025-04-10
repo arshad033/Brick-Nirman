326a254dd05dc07a3aef50f35ba3600da283a98e
arshad033 <arshadobra85@gmail.com>
2025-04-10 16:41:09 +0530

    style: format order.routes.js for consistent code style and readability

--- Server/src/routes/order.routes.js
+++ Server/src/routes/order.routes.js
@@ -1,13 +1,20 @@
-import { Router } from "express";
-const router = Router()
-import { createOrder, getAllOrders, getOrderById, updateOrder, cancelOrder, updateOrderStatus } from "../controllers/order.controller.js";
-import { verifyJwt } from "../middlewares/auth.middleware.js";
+import { Router } from 'express';
+const router = Router();
+import {
+  createOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrder,
+  cancelOrder,
+  updateOrderStatus,
+} from '../controllers/order.controller.js';
+import { verifyJwt } from '../middlewares/auth.middleware.js';
 
-router.use(verifyJwt)
-router.route("/create-order").post(createOrder);
-router.route("/get-All-Orders").get(getAllOrders);
-router.route("/update-orders/:id").patch(updateOrder);
-router.route("/cancel-order/:id").delete(cancelOrder);
-router.route("/get-order/:id").get(getOrderById);
+router.use(verifyJwt);
+router.route('/create-order').post(createOrder);
+router.route('/get-All-Orders').get(getAllOrders);
+router.route('/update-orders/:id').patch(updateOrder);
+router.route('/cancel-order/:id').delete(cancelOrder);
+router.route('/get-order/:id').get(getOrderById);
 
 export default router;
