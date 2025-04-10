da56b332ab43b1a3dd91996474b696cbd8ea7169
arshad033 <arshadobra85@gmail.com>
2025-04-10 23:47:10 +0530

    fix: correct CORS origin URL by removing trailing slash

--- Server/src/app.js
+++ Server/src/app.js
@@ -12,7 +12,7 @@ import supplierRouter from './routes/supplier.routes.js';
 app.use(
   cors({
     origin:
-      'https://brick-nirman-frontend-a6p8fffoc-arshad033s-projects.vercel.app/' ||
+      'https://brick-nirman-frontend-a6p8fffoc-arshad033s-projects.vercel.app' ||
       process.env.CORS_ORIGIN,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
